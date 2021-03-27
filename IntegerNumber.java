e059f08bcaabb4c2ea0446344aab319ea8132633
gauravchoudhary27 <gc81470@gmail.com>
2021-03-27 23:23:11 +0530

    Print the integer value from user

---
+++ IntegerNumber.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class IntegerNumber {
+    public static void main(String[] args) {
+        Scanner Sc=new Scanner(System.in);
+        System.out.println("please enter the integer value");
+        int X=Sc.nextInt();
+        System.out.println(X);
+
+    }
+
+}
